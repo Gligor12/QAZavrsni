1f274c54797ccdaf59db8fe21be1feae6f8ba6d4
unknown <cicmilgligor123@gmail.com>
2024-02-11 16:31:27 +0100

    delete board with cy.request

---
+++ cypress/e2e/deleteBoardViaApi.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="Cypress" />
+
+import { loginUser } from "../page_objects/loginPage"; 
+import { createBoard } from "../page_objects/createBoardPage";
+
+let boardId;
+let token;
+describe("Delete board Via API",()=> {
+    
+    before( ()=>{
+        cy.visit("/login");
+        loginUser.loginUserFunc("gligor123@gmail.com" , "gligor12345");
+        
+        cy.get("li[title='Add new Board']").click();
+        cy.intercept(
+            "POST",
+            "https://cypress-api.vivifyscrum-stage.com/api/v2/boards"
+        ).as("createBoard");
+        
+        
+       
+        createBoard.createBoardFunc("New Board");
+
+        cy.wait("@createBoard").then(intercept =>{
+            boardId=intercept.response.body.id;
+            token = window.localStorage.getItem("token");
+        });
+       
+    });
+
+    it("Delete board", ()=>{
+
+       
+        cy.request({
+            url: `https://cypress-api.vivifyscrum-stage.com/api/v2/boards/${boardId}`,
+            method: "DELETE",
+            auth: {
+                "bearer" : token
+            }
+        }).its("body").then(resp =>{
+            expect(resp.id).eq(boardId);
+        });
+    });
+});
